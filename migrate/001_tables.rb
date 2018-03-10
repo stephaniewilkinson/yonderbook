f2916a2afa34cfea6271f728b01e1e750f9135dd
Stephanie Wilkinson <what.happens@gmail.com>
2018-03-10 13:44:42 -0800

    created user table

--- migrate/001_tables.rb
+++ migrate/001_tables.rb
@@ -1,7 +1,7 @@
-Sequel.migration do
-  change do
-    create_table(:users) do
-      primary_key :id
-    end
-  end
+require_relative "../db.rb"
+
+DB.create_table(:users) do
+  primary_key :id
+  String :email, uniq: true, null: false
+  Integer :goodreads_user_id
 end
