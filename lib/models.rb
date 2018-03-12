4b171889596bd265cb7e597865fe206c4a98231f
Stephanie Wilkinson <what.happens@gmail.com>
2018-03-11 18:47:10 -0700

    reformat

--- lib/models.rb
+++ lib/models.rb
@@ -12,8 +12,7 @@ if ENV['RACK_ENV'] == 'development'
     require 'rack/unreloader'
     Unreloader = Rack::Unreloader.new(:reload=>false)
   end
-
-  Unreloader.require('models'){|f| Sequel::Model.send(:camelize, File.basename(f).sub(/\.rb\z/, ''))}
+  Unreloader.require('models') { |f| Sequel::Model.send :camelize, File.basename(f).sub(/\.rb\z/, '') }
 
   require 'logger'
   DB.loggers << Logger.new($stdout)
