192e1d0e407432625fe496f6e6bee3ab8458c363
Stephanie Wilkinson <what.happens@gmail.com>
2018-04-14 16:34:58 -0700

    see if codeship raises erros

--- app.rb
+++ app.rb
@@ -61,6 +61,7 @@ class App < Roda
     end
 
     r.on 'login' do
+      raise 'string'
       r.get do
         r.redirect '/shelves/index'
       end
@@ -74,7 +75,6 @@ class App < Roda
         else
           access_token = cache_get(:request_token).get_access_token
           user_id, first_name = Goodreads.fetch_user access_token
-          session = nil
           session[:goodreads_user_id] = user_id
           # @users.insert_conflict.insert(first_name: first_name, goodreads_user_id: user_id)
         end
