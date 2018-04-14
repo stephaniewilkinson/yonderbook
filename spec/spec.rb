59236ee1de512ed2f217d5599bf3b3aa79dc2f0e
Stephanie Wilkinson <what.happens@gmail.com>
2018-04-14 14:46:35 -0700

    set cookie

--- spec/spec.rb
+++ spec/spec.rb
@@ -12,7 +12,9 @@ describe App do
   end
 
   headers = {}
-  # Rack::Utils.set_cookie_header!(headers, key, value)
+
+  Rack::Utils.set_cookie_header!(headers, :secret, Goodreads::SECRET)
+  Rack::Utils.set_cookie_header!(headers, :api_key, Goodreads::API_KEY)
   cookie_string = headers['Set-Cookie']
   Capybara.current_session.driver.browser.set_cookie(cookie_string)
 
