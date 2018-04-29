26d1d3f4f92f551dbe437ac4b9df2bb6f007cac2
Stephanie Wilkinson <what.happens@gmail.com>
2018-04-29 14:03:28 -0700

    needs refactoring

--- lib/overdrive.rb
+++ lib/overdrive.rb
@@ -7,36 +7,40 @@ module Overdrive
   KEY        = ENV.fetch 'OVERDRIVE_KEY'
   SECRET     = ENV.fetch 'OVERDRIVE_SECRET'
 
-  Title = Struct.new :title, :image, :copies_available, :copies_owned, :isbn, :url, keyword_init: true
+  Title = Struct.new :title, \
+                     :image, \
+                     :copies_available, \
+                     :copies_owned, \
+                     :isbn, \
+                     :url, \
+                     :id, \
+                     :availability_url, \
+                     keyword_init: true
 
   module_function
 
-  def fetch_products_info book, collection_token, token
-    availability_uri = "#{Overdrive::API_URI}/collections/#{collection_token}/products?q=#{URI.encode("\"#{book[2]}\"")}"
-    response = HTTP.auth("Bearer #{token}").get(availability_uri)
-    JSON.parse(response.body)['products']
-  end
-
   def fetch_titles_availability isbnset, collection_token, token
-    isbnset.map do |book|
-
-      products = fetch_products_info book, collection_token, token
-
-      if products
-        availibility_url = products.first['links'].assoc('availability').last['href']
-        response = HTTP.auth("Bearer #{token}").get(availibility_url)
-        book_body = JSON.parse(response.body)
-        copies_available = book_body['copiesAvailable']
-        copies_owned = book_body['copiesOwned']
-        url = products.first['contentDetails'].first['href']
-      end
+    books = isbnset.map do |book|
+      availability_url = "#{Overdrive::API_URI}/collections/#{collection_token}/products?q=#{URI.encode("\"#{book[2]}\"")}"
+      response = HTTP.auth("Bearer #{token}").get(availability_url)
+      products = JSON.parse(response.body)['products']
 
       Title.new isbn: book[0],
                 image: book[1],
                 title: book[2],
-                copies_available: copies_available || 0,
-                copies_owned: copies_owned || 0,
-                url: url
+                id: products&.dig(0, 'crossRefId'),
+                availability_url: products&.dig(0, 'links')&.assoc('availability')&.dig(-1, 'href'),
+                url: products&.dig(0, 'contentDetails', 0, 'href')
+    end
+
+    books.map do |book|
+      next book unless book.id
+
+      response = HTTP.auth("Bearer #{token}").get(book.availability_url)
+      book_body = JSON.parse(response.body)
+      book.copies_available = book_body['copiesAvailable'] || 0
+      book.copies_owned = book_body['copiesOwned'] || 0
+      book
     end
   end
 end
