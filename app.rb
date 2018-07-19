63509e642465b8c87db742bd7feee56ef3bfcfd4
Stephanie Wilkinson <what.happens@gmail.com>
2018-07-18 17:51:47 -0700

    cops

--- app.rb
+++ app.rb
@@ -51,7 +51,7 @@ class App < Roda
 
       # route: GET /
       r.get do
-        view 'welcome' # renders views/welcome.erb inside views/layout.erb
+        view 'welcome'
       end
     end
 
@@ -218,6 +218,7 @@ class App < Roda
             status, book = Goodreads.fetch_book_data isbn
 
             raise "#{status}: #{book}" unless status == :ok
+
             @books.insert isbn: isbn, user_id: user[:id], cover_image_url: book.image_url, title: book.title
           end
           r.redirect '/inventory/index'
--- lib/mail.rb
+++ lib/mail.rb
@@ -18,6 +18,6 @@ module Mail
     content = Content.new(type: 'text/html', value: @email_template)
     mail = Mail.new(from, subject, to, content)
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    sg.client.mail._('send').post(request_body: mail.to_json)
   end
 end
