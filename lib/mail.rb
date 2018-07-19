bed710cb233b67a0cc4fcd084d6205a89d87f9c4
Stephanie Wilkinson <what.happens@gmail.com>
2018-07-18 17:47:23 -0700

    remove logs

--- lib/mail.rb
+++ lib/mail.rb
@@ -19,8 +19,5 @@ module Mail
     mail = Mail.new(from, subject, to, content)
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.headers
   end
 end
