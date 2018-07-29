e3901261058857fa083188fec8479feee8fe4df2
Stephanie Wilkinson <what.happens@gmail.com>
2018-07-28 18:31:59 -0700

    fix the order of the params

--- lib/bookmooch.rb
+++ lib/bookmooch.rb
@@ -9,18 +9,18 @@ module Bookmooch
 
   def books_added_and_failed isbns_and_image_urls, username, password
     hydra = Typhoeus::Hydra.new
-    requests_images_and_titles = isbns_and_image_urls.map do |isbn, image_url, title|
+    requests_titles_and_images = isbns_and_image_urls.map do |isbn, image_url, title|
       params = {asins: isbn, target: 'wishlist', action: 'add'}
       request = Typhoeus::Request.new "#{BASE_URL}/api/userbook", params: params, username: username, password: password
       hydra.queue request
-      [request, image_url, title]
+      [request, title, image_url]
     end
     hydra.run
 
-    added_or_failed = requests_images_and_titles.partition do |request, _, _|
+    added_or_failed = requests_titles_and_images.partition do |request, _, _|
       request.response.body.to_s.strip == request.options[:params][:asins]
     end
 
-    added_or_failed.map { |partition| partition.map { |_, image_url, title| [image_url, title] } }
+    added_or_failed.map { |partition| partition.map { |_, title, image_url| [title, image_url] } }
   end
 end
