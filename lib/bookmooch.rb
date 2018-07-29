afdea902203cf4625b5348db6b9b55d1b4959fa1
Stephanie Wilkinson <what.happens@gmail.com>
2018-07-28 17:58:45 -0700

    refactor overdrive to use hydra

--- lib/bookmooch.rb
+++ lib/bookmooch.rb
@@ -8,21 +8,19 @@ module Bookmooch
   module_function
 
   def books_added_and_failed isbns_and_image_urls, username, password
-    books_added = []
-    books_failed = []
-
-    isbns_and_image_urls.each do |isbn, image_url, title|
+    hydra = Typhoeus::Hydra.new
+    requests_images_and_titles = isbns_and_image_urls.map do |isbn, image_url, title|
       params = {asins: isbn, target: 'wishlist', action: 'add'}
+      request = Typhoeus::Request.new "#{BASE_URL}/api/userbook", params: params, username: username, password: password
+      hydra.queue request
+      [request, image_url, title]
+    end
+    hydra.run
 
-      response = Typhoeus.get "#{BASE_URL}/api/userbook", params: params, username: username, password: password
-
-      if response.body.to_s.strip == isbn
-        books_added << [title, image_url]
-      else
-        books_failed << [title, image_url]
-      end
+    added_or_failed = requests_images_and_titles.partition do |request, image_url, title|
+      request.response.body.to_s.strip == request.options[:params][:asins]
     end
 
-    [books_added, books_failed]
+    added_or_failed.map { |partition| partition.map { |_, image_url, title| [image_url, title] } }
   end
 end
