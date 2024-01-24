34a7672f8ed04b8b36775148c9d1cf99650c025d
Stephanie Wilkinson <what.happens@gmail.com>
2024-01-24 15:31:19 -0800

    remove line to connect to db

--- lib/db.rb
+++ lib/db.rb
@@ -5,4 +5,4 @@ require 'sequel'
 
 # Delete DATABASE_URL from the environment, so it isn't accidently
 # passed to subprocesses.  DATABASE_URL may contain passwords.
-DB = Sequel.connect ENV.delete 'DATABASE_URL'
+# DB = Sequel.connect ENV.delete 'DATABASE_URL'
