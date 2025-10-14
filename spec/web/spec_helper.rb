a537b3f6e0d9c34bc6c95a57f500b9276f0cfd6d
Stephanie Wilkinson <what.happens@gmail.com>
2025-10-14 06:25:17 -0700

    Update webdriver config

--- spec/web/spec_helper.rb
+++ spec/web/spec_helper.rb
@@ -22,15 +22,20 @@ Capybara.register_driver :firefox do |app|
 end
 
 Capybara.register_driver :headless_firefox do |app|
-  Capybara::Selenium::Driver.new app, browser: :firefox
+  options = Selenium::WebDriver::Firefox::Options.new
+  options.add_argument('--headless')
+  Capybara::Selenium::Driver.new app, browser: :firefox, options: options
 end
 
-Capybara.javascript_driver = :firefox
+# Use headless Firefox in CI environments
+driver = ENV['CI'] ? :headless_firefox : :firefox
+
+Capybara.javascript_driver = driver
 
 Capybara.configure do |config|
   config.server = :falcon
   config.run_server = true
   config.server_port = 9292
-  config.default_driver = :firefox
+  config.default_driver = driver
   config.app_host = 'http://localhost:9292'
 end
